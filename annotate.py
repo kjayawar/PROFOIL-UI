248971df5f555b0116ed0e8443ae5f2446f2cfa9
kjayawar <kjayawar@gmail.com>
2024-10-02 15:36:41 +1000

    'annotate_text'

--- annotate.py
+++ annotate.py
@@ -27,16 +27,16 @@ IFTP1_dict = {
 	101 : "c_m0",
 	102 : "t/c_max",
 	103 : "alpha_0 (rarely used)",
-	104 : "xc_max location of maximum thickness",
+	104 : "x/c_max location of maximum thickness",
 	105 : "maximum camber",
 	106 : "leading edge radius",
-	107 : "t/c_max according to Eppler code",
+	107 : "t/c_max (Eppler method)",
 	121 : "t/c_max for a gensym airfoil",
 	122 : "flap angle for a gensym airfoil",
 	135 : "area (uses value for CHORDO for chord length)",
 	146 : "volume of an axisymmetric body produced by a symmetric airfoil rotated about the x-axis",
 	155 : "TEX (Trailing Edge offset in X)",
-	156 : "TEY (Trailing Edge offset in Y, i.e., TE thickness)",
+	156 : "TEY (Trailing Edge offset in Y)",
 	157 : "XTEMIDPT (Trailing Edge MIDPoinT in X -- relative to x = 1)",
 	158 : "YTEMIDPT (Trailing Edge MIDPoinT in Y)",
 	184 : "omega_us",
@@ -51,9 +51,9 @@ IFTP1_dict = {
 	197 : "omega'_ls",
 	198 : "omega_T_us (incompatible with IFTP1=200)",
 	199 : "omega_T_ls (incompatible with IFTP1=200)",
-	201 : "velocity differential between phi_s_us and phi_s_ls",
-	202 : "velocity at phi_s_us",
-	203 : "velocity at phi_s_ls",
+	201 : "velocity differential between Phi_S_us and Phi_S_ls",
+	202 : "velocity at Phi_S_us",
+	203 : "velocity at Phi_S_ls",
 	204 : "c_m",
 	205 : "t/c for given x/c",
 	206 : "camber for given x/c",
@@ -61,35 +61,35 @@ IFTP1_dict = {
 	401 : "s/c",
 	402 : "y/c",
 	403 : "angle (used to design flat bottomed airfoils)",
-	500 : "H_12 when AoA is segment alpha*",
-	501 : "R_delta2 when AoA is segment alpha*",
-	502 : "TC when AoA is segment alpha*",
-	503 : "n_ENN when AoA is segment alpha*",
-	504 : "n_Drela when AoA is segment alpha*",
-	505 : "H_32 when AoA is segment alpha*",
-	506 : "c_f when AoA is segment alpha*",
+	500 : "H_12 when alpha is segment alpha*",
+	501 : "R_delta2 when alpha is segment alpha*",
+	502 : "TC when alpha is segment alpha*",
+	503 : "n_ENN when alpha is segment alpha*",
+	504 : "n_Drela when alpha is segment alpha*",
+	505 : "H_32 when alpha is segment alpha*",
+	506 : "c_f when alpha is segment alpha*",
 	600 : "H_12",
 	601 : "H_32",
 }
 
 IFTP2_dict = {
-	100 : "v(s) when AoA is segment alpha*",
-	200 : "H_12(s) when AoA is segment alpha*",
-	201 : "TC(s) when AoA is segment alpha*",
-	202 : "n(s)_ENN when AoA is segment alpha*",
-	203 : "n(s)_Drela when AoA is segment alpha*",
-	204 : "H_32(s) when AoA is segment alpha*",
-	205 : "C_f(s) when AoA is segment alpha*",
+	100 : "v(s) when alpha is segment alpha*",
+	200 : "H_12(s) when alpha is segment alpha*",
+	201 : "TC(s) when alpha is segment alpha*",
+	202 : "n(s)_ENN when alpha is segment alpha*",
+	203 : "n(s)_Drela when alpha is segment alpha*",
+	204 : "H_32(s) when alpha is segment alpha*",
+	205 : "C_f(s) when alpha is segment alpha*",
 	300 : "H_32(s) for given Re(R1) and alpha(R2) --[allows for iteration on alpha*]",
 	500 : "Bubble ramp"
 }
 
 #======================================== ITP.. Definitions =========================================
 ITP_1_val_dict ={
-	1 : "Phi of seg {}",
-	4 : "Vi (iteration on velocity level allowed only once) of seg {}",
-	5 : "Vi_tilde of seg {}",
-	6 : "alpha* of seg {}",
+	1 : "Phi for segment {}",
+	4 : "Vi (iteration on velocity level allowed only once) for segment {}",
+	5 : "Vi_tilde for segment {}",
+	6 : "Alpha* for segment {}",
 }
 
 ITP_2_val_dict = {
@@ -101,52 +101,56 @@ ITP_2_val_dict = {
 	},
 
 	1:{
-	100 : "Upper-surface Phi-values, except Phi_ILE",
-	200 : "Lower-surface Phi-values, except Phi_ILE",
-	300 : "Upper-surface and lower-surface Phi-values in OPPOSITE directions, except Phi_ILE",
-	400 : "Upper-surface and lower-surface Phi-values in the SAME direction, except Phi_ILE",	
-	500 : "Upper-surface Phi-values, including Phi_ILE",
-	600 : "Lower-surface Phi-values, including Phi_ILE",
-	700 : "Upper-surface and lower-surface Phi-values in OPPOSITE directions, including Phi_ILE",
-	800 : "Upper-surface and lower-surface Phi-values in the SAME direction, including Phi_ILE",
-	1000 : "Group 1 Phi-values",
-	1100 : "Group 2 Phi-values",
-	1200 : "Group 3 Phi-values",
+	100 : "upper-surface Phi-values, except Phi_ILE",
+	200 : "lower-surface Phi-values, except Phi_ILE",
+	300 : "upper-surface and lower-surface Phi-values in OPPOSITE directions, excluding Phi_ILE",
+	400 : "upper-surface and lower-surface Phi-values in the SAME direction, excluding Phi_ILE",	
+	500 : "upper-surface Phi-values, including Phi_ILE",
+	600 : "lower-surface Phi-values, including Phi_ILE",
+	700 : "upper-surface and lower-surface Phi-values in OPPOSITE directions, including Phi_ILE",
+	800 : "upper-surface and lower-surface Phi-values in the SAME direction, including Phi_ILE",
+	1000 : "group 1 Phi-values",
+	1100 : "group 2 Phi-values",
+	1200 : "group 3 Phi-values",
 	},
 	
 	2:{
-	100 : "Phi_W Upper(= Phi1)",
-	200 : "Phi_W Lower(= PhiISEG−1)",
+	100 : "Phi_W Upper (=Phi_1)",
+	200 : "Phi_W Lower (=Phi_ISEG-1)",
 	300 : "Phi_W Upper and Phi_W Lower in OPPOSITE directions",
 	400 : "Phi_W Upper and Phi_W Lower in the SAME direction",
 	},
 	3:{
 	100 : "Phi_S Upper(= Phi1)",
-	200 : "Phi_S Lower(= PhiISEG−1)",
+	200 : "Phi_S Lower(= PhiI_SEG-1)",
 	300 : "Phi_S Upper and Phi_S Lower in OPPOSITE directions",
 	400 : "Phi_S Upper and Phi_S Lower in the SAME direction",
 	},
 	6:{
-	100 : "Upper-surface alpha* values",
-	200 : "Lower-surface alpha* values",
-	300 : "Upper-surface alpha* values and lower-surface alpha* values in OPPOSITE directions",
-	400 : "Upper-surface alpha* values and lower-surface alpha* values in the SAME direction",
-	1000 : "Group 1 alpha*-values",
-	1100 : "Group 2 alpha*-values",
-	1200 : "Group 3 alpha*-values",
+	100 : "upper-surface alpha* values",
+	200 : "lower-surface alpha* values",
+	300 : "upper-surface alpha* values and lower-surface alpha* values in OPPOSITE directions",
+	400 : "upper-surface alpha* values and lower-surface alpha* values in the SAME direction",
+	500 : "upper-surface alpha* values (except not recovery alpha*)",
+	600 : "lower-surface alpha* values (except not recovery alpha*)",
+	700 : "upper-surface alpha* values and lower-surface alpha* values in OPPOSITE directions (except not recovery alpha*s)",
+	800 : "upper-surface alpha* values and lower-surface alpha* values in the SAME direction (except not recovery alpha*s)",
+	1000 : "group 1 alpha*-values",
+	1100 : "group 2 alpha*-values",
+	1200 : "group 3 alpha*-values",
 	},
 	7:{
-	100 : "Upper-surface K",
-	200 : "Lower-surface K",
-	300 : "Upper-surface K and Lower-surface K in OPPOSITE directions",
-	400 : "Upper-surface K and Lower-surface K in the SAME direction",
+	100 : "upper-surface K",
+	200 : "lower-surface K",
+	300 : "upper-surface K and Lower-surface K in OPPOSITE directions",
+	400 : "upper-surface K and Lower-surface K in the SAME direction",
 	}
 }
 
 #====================================== LLBOS/LLBE Definitions ======================================
 LLBOS_dict = {
-	1: "BEGGINING of the segment",
-	0: "END of the segment"
+	1: "beginning of the segment (BOS)",
+	0: "end of the segment (EOS)"
 }
 
 LLBE_dict = {
@@ -156,49 +160,49 @@ LLBE_dict = {
 
 # ======================================= NEWT1.. Descriptions =======================================
 NEWT1G0_description = """
-# Specify {} => {} 
-# By iterating on {}.
+# Specify {} = {} 
+# Iterate on {}.
 {}{}"""
 
 NEWT1G1_description = """
-# Specify {} => {} @ {} 
-# By iterating on {}.
+# Specify {} = {} @ {} 
+# Iterate on {}.
 {}{}"""
 
 NEWT1S0_description = """
-# Specify {} at the {} {} => {} 
-# By iterating on {}.
+# Specify {} at the {} {} = {} 
+# Iterate on {}.
 {}{}"""
 
 NEWT1S1_description = """
-# Specify {} => {} at the {} {} for Re={}
-# By iterating on {}.
+# Specify {} = {} at the {} {} for Re={}
+# Iterate on {}.
 {}{}"""
 
 NEWT1S2_description = """
-# Specify {} => {} at the {} {} @ AoA of {} degrees and Re={} 
-# By iterating on {}.
+# Specify {} = {} at the {} {} @ alpha of {} degrees and Re={} 
+# Iterate on {}.
 {}{}"""
 
 # ======================================= NEWT2.. Descriptions =======================================
 
 NEWT2SD0_description = """
 # Specify {}
-# By iterating DELV segment {} velocity distribution
+# Iterate on DELV for segment {} velocity distribution
 # Specification(s) given by the [SubSegment arc length | SubSegment tilde] pairs in the proceeding {}; 
 # Specs are {}
 {}{}"""
 
 NEWT2SD1_description = """
 # Specify {}
-# By iterating DELV segment {} velocity distribution
+# Iterating on DELV for segment {} velocity distribution
 # Specification(s) given by the [SubSegment arc length | SubSegment tilde] pairs in the proceeding {}; 
 # Specs are {} for given Re = {}
 {}{}"""
 
 NEWT2SD2_description = """
 # Specify {}
-# By iterating DELV segment {} velocity distribution
+# Iterate on DELV for segment {} velocity distribution
 # Specification(s) given by the [SubSegment arc length | SubSegment tilde] pairs in the proceeding {}; 
 # Specs are {} for given Re = {} and alpha = {}
 {}{}"""
@@ -211,8 +215,8 @@ NEWT1S1 = namedtuple('NEWT1S1', "IFTP1 JSEGIX1 LLBOS COND1 FNEWT1 ITP1 ITP2 CLAM
 NEWT1S2 = namedtuple('NEWT1S2', "IFTP1 JSEGIX1 LLBOS COND1 COND2 FNEWT1 ITP1 ITP2 CLAMP1",defaults=(None,)*9)
 
 # ======================================= NEWT2.. Named tuples =======================================
-NEWT2SD0 = namedtuple('NEWT2SD0', "IFTP2 JSEGIX2 LLBE KADJSBS CLAMP1",        defaults=(None,)*5) 				                   
-NEWT2SD1 = namedtuple('NEWT2SD1', "IFTP2 JSEGIX2 LLBE R1 KADJSBS CLAMP1",     defaults=(None,)*6)		
+NEWT2SD0 = namedtuple('NEWT2SD0', "IFTP2 JSEGIX2 LLBE KADJSBS CLAMP1",        defaults=(None,)*5)
+NEWT2SD1 = namedtuple('NEWT2SD1', "IFTP2 JSEGIX2 LLBE R1 KADJSBS CLAMP1",     defaults=(None,)*6)
 NEWT2SD2 = namedtuple('NEWT2SD2', "IFTP2 JSEGIX2 LLBE R1 R2 KADJSBS CLAMP1",  defaults=(None,)*7)
 
 # ================================== NEWT1.. Interpreter Functions ===================================
@@ -232,7 +236,7 @@ def interpret_NEWT1G1(newt_line):
 	newt1g1 = NEWT1G1(*newt_line.split()[1:])
 	return NEWT1G1_description.format(IFTP1_dict[int(newt1g1.IFTP1)],
 									  newt1g1.FNEWT1,
-									  f"x/c of {newt1g1.COND1}" if newt1g1.IFTP1=="205" else f"AoA of {newt1g1.COND1} degrees",
+									  f"x/c of {newt1g1.COND1}" if newt1g1.IFTP1=="205" else f"alpha of {newt1g1.COND1} degrees",
 									  get_ITP_interpretation(int(newt1g1.ITP1), int(newt1g1.ITP2)),
 									  f"# CLAMP is {newt1g1.CLAMP1}\n" if newt1g1.CLAMP1 else "",
 									  newt_line)
